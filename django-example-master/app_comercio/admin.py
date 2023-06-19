8ea90c88df8ece98f1baa0ef7d12ec16a87b9b9a
AntoAGA <133665472+AntoAGA@users.noreply.github.com>
2023-06-19 17:03:41 -0400

    Delete django-example-master directory

--- django-example-master/app_comercio/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Cliente, Mouse, Monitor, Computador
-admin.site.register(Cliente)
-admin.site.register(Mouse)
-admin.site.register(Monitor)
-admin.site.register(Computador)
